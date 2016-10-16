30eb0b60c69d4ca0f1436abe0ff1ff9dda4e90b7
Sergey <sergeygerasimov778@gmail.com>
2016-10-16 21:48:13 +0300

    Add simple sieve and test for it

--- lib/PrimeSearcher/PrimeSearcher.cs
+++ lib/PrimeSearcher/PrimeSearcher.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace Primes
 {
     public class PrimeSearcher
     {
-        
+        public static List<int> Sieve(int highBound)
+        {
+            var result = new List<int>();
+            if (highBound <= 1) return result;
+            var primes = new BitArray(highBound, true);
+            for (int i = 2; i < highBound; i++)
+            {
+                long square = 1L * i * i;
+                if (primes.Get(i))
+                {
+                    for (long j = square; j < highBound; j += i)
+                    {
+                        primes.Set((int) j, false);
+                    }
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
     }
 }
--- test/PrimeSearcher.Tests/Tests.cs
+++ test/PrimeSearcher.Tests/Tests.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using Xunit;
+using Primes;
 
 namespace Tests
 {
     public class Tests
     {
         [Fact]
-        public void Test1() 
+        public void TestFirstPrimes() 
         {
-            Assert.True(true);
+            var list = PrimeSearcher.Sieve(200);
+            var truePrimes = new int[] {
+                2, 3, 5, 7, 11, 13, 17, 
+                19, 23, 29, 31, 37, 41, 
+                43, 47, 53, 59, 61, 67, 
+                71, 73, 79, 83, 89, 97, 
+                101, 103, 107, 109, 113, 
+                127, 131, 137, 139, 149, 
+                151, 157, 163, 167, 173, 
+                179, 181, 191, 193, 197, 199
+            };
+            for (int i = 0; i < list.Count; i++) 
+            {
+                Assert.True(list[i] == truePrimes[i], $"{i} position prime must be {truePrimes[i]}. Actual: {list[i]}");
+            }
+        }
+
+        [Fact]
+        public void TestPrimesCount()
+        {
+            var list = PrimeSearcher.Sieve(100000);
+            var trueCount = 9592;
+            Assert.True(list.Count == trueCount, $"Primes count before 100000 is {trueCount}. Actual: {list.Count}");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10000)]
+        [InlineData(0)]
+        public void TestNegativeParameter(int arg) 
+        {
+            var list = PrimeSearcher.Sieve(arg);
+            Assert.True(list.Count == 0, $"Primes count with negative argument must be 0. Actual: {list.Count}");
         }
     }
 }
