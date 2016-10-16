10f71f80c30d0bb21518cdab09f2b50e6bad698e
Sergey <sergeygerasimov778@gmail.com>
2016-10-16 21:56:13 +0300

    Create Sieve implementation that returns bit array

--- lib/PrimeSearcher/PrimeSearcher.cs
+++ lib/PrimeSearcher/PrimeSearcher.cs
@@ -6,10 +6,10 @@ namespace Primes
 {
     public class PrimeSearcher
     {
-        public static List<int> Sieve(int highBound)
+        protected static Tuple<List<int>, BitArray> SieveImplementation(int highBound)
         {
             var result = new List<int>();
-            if (highBound <= 1) return result;
+            if (highBound <= 1) return new Tuple<List<int>, BitArray>(result, new BitArray(0));
             var primes = new BitArray(highBound, true);
             for (int i = 2; i < highBound; i++)
             {
@@ -23,7 +23,9 @@ namespace Primes
                     result.Add(i);
                 }
             }
-            return result;
+            return new Tuple<List<int>, BitArray>(result, primes);
         }
+
+        public static List<int> Sieve(int highBound) => SieveImplementation(highBound).Item1;
     }
 }
